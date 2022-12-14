96746595d5c4ff935e04519bb9606b9715557b90
ErmalKeqmezi <ekeqmezi10@gmail.com>
2022-12-14 23:32:10 +0100

    commit

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
-const loginForm= document.querySelector("form.login");
-const signupForm= document.querySelector("form.signup");
-const loginBtn= document.querySelector("label.login");
-const signupBtn= document.querySelector("label.signup");
-const loginText= document.querySelector(".login");
-const signupText= document.querySelector(".signup");
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const loginText = document.querySelector(".login");
+const signupText = document.querySelector(".signup");
 
 signupBtn.onclick = () => {
     loginForm.style.marginLeft = "-50%";
